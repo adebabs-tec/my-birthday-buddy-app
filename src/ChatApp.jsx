7a1263ff076c0133128e9962754f389830084442
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-09-04 08:00:54 +0100

    remainResp

--- src/ChatApp.jsx
+++ src/ChatApp.jsx
@@ -1,16 +1,35 @@
 // import React from 'react'
+import { useState } from 'react'
 import Chats from './chat'
 
 export default function ChatApp() {
-  const chatList = Chats.map((chat) => {
+  const [chats, setChats] = useState(Chats)
+  const chatList = chats.map((chat) => {
     const { id, img, name, age } = chat
     return (
-      <li key={id}>
-        <img src={img} />
-        <h1>{name}</h1>
-        <h3>{age}</h3>
-      </li>
+      <>
+        <li key={id}>
+          <img src={img} />
+          <div className="user-details">
+            <h1>{name}</h1>
+            <h3>{age}</h3>
+          </div>
+        </li>
+      </>
     )
   })
-  return <ul>{chatList}</ul>
+  return (
+    <ul>
+      <h3>{chats.length} Birthday Today</h3>
+      {chatList}
+      <button
+        className="btn"
+        onClick={() => {
+          setChats([])
+        }}
+      >
+        Clear All
+      </button>
+    </ul>
+  )
 }
